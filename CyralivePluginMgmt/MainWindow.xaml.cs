b59542793a5d8103cca2c783000d9781b9c175f1
Eric Ren <ericrren@outlook.com>
2025-04-27 20:17:37 +0800

    Add plugin manager

---
+++ CyralivePluginMgmt/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ListViewItem = System.Windows.Controls.ListViewItem;
+using MessageBox = System.Windows.Forms.MessageBox;
+
+namespace CyralivePluginMgmt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<string> strings = new List<string>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            System.Windows.Forms.Application.EnableVisualStyles();
+            if (Directory.Exists("..\\resources\\plugins") && Directory.GetDirectories("..\\resources\\plugins").Length > 0)
+            {
+                foreach (string folder_path in Directory.GetDirectories("..\\resources\\plugins"))
+                {
+                    strings.Add(folder_path);
+                    Assembly assembly = Assembly.Load(File.ReadAllBytes(folder_path + "\\" + Regex.Split(folder_path, @"\\").Last() + ".dll"));
+                    ListViewItem listViewItem = new ListViewItem();
+                    foreach (Type type in assembly.GetExportedTypes())
+                    {
+                        if (type.Name == "plugin_base")
+                        {
+                            listViewItem.Content = type.InvokeMember("pluginName", BindingFlags.InvokeMethod, null, Activator.CreateInstance(type), null) as string;
+                        }
+                    }
+                    plugin_list.Items.Add(listViewItem);
+                }
+            }
+        }
+
+        private void close_plugin_mgmt_Click(object sender, RoutedEventArgs e)
+        {
+            /*Process process = new Process();
+            process.StartInfo.FileName = Directory.GetCurrentDirectory() + "\\..\\OpenCyralive.exe";
+            process.Start();*/
+            Close();
+        }
+
+        private void del_plugin_Click(object sender, RoutedEventArgs e)
+        {
+            if (plugin_list.SelectedItem != null)
+            {
+                try
+                {
+                    Directory.Delete(strings[plugin_list.SelectedIndex], true);
+                    strings.Clear();
+                    foreach (string folder_path in Directory.GetDirectories("..\\resources\\plugins"))
+                    {
+                        strings.Add(folder_path);
+                    }
+                    MessageBox.Show((string)((ListViewItem)plugin_list.SelectedItem).Content + " 已被删除。", "操作成功", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    plugin_list.Items.Remove(plugin_list.SelectedItem);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, ex.Source, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void install_oc_plugin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
