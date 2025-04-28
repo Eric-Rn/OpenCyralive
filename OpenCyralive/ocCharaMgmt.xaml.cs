082faf0e3a9de7ea3486766ddaed269593869101
Eric Ren <ericrren@outlook.com>
2025-04-28 16:13:21 +0800

    Delete unneeded code

--- OpenCyralive/ocCharaMgmt.xaml.cs
+++ OpenCyralive/ocCharaMgmt.xaml.cs
@@ -1,20 +1,12 @@
 ﻿using Microsoft.Win32;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using static OpenCyralive.GlobalFunction;
 using MessageBox = System.Windows.Forms.MessageBox;
 
